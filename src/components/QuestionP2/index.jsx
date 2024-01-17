51c98e294067c7342dc6c13ef326c02fa846e57e
prof <spasmodic51@gmail.com>
2024-01-17 20:44:56 +0100

    P2 intégré! Passage au P3

--- src/components/QuestionP2/index.jsx
+++ src/components/QuestionP2/index.jsx
@@ -1,15 +1,24 @@
-
 import "../../style/components/QuestionP2/questionP2.scss"
+import { useState } from "react"
 const QuestionP2 = (Props) => {
+
+const [style, updateStyle] = useState(false);
+const Reset = () => {
+    if(style) {
+updateStyle(false);}
+else {
+    updateStyle(true)
+}
+}
 
     return(
         <div>
-             <div className="P2-question">
+             <div className={style? "P2-question" : "P2-question reset"} id={"q"+Props.id} onClick={Reset} >
       <h2 className="P2-h2">{Props.title}</h2>
-      <div className="P2-input"><input type="radio" id="number1" name={Props.id} value="a" checked /><label htmlFor="number1">{Props.rep1}</label></div>
-      <div className="P2-input"><input type="radio" id="number2" name={Props.id} value="b" /><label htmlFor="number2">{Props.rep2}</label>
+      <div className="P2-input"><input  type="radio" id={Props.id+"number1"} name={Props.id} value="a" defaultChecked onChange={Props.function} /><label htmlFor={Props.id+"number1"}>{Props.rep1}</label></div>
+      <div className="P2-input"><input type="radio" id={Props.id+"number2"} name={Props.id} value="b" onChange={Props.function} /><label htmlFor={Props.id+"number2"}>{Props.rep2}</label>
       </div>
-      <div className="P2-input"><input type="radio" id="number3" name={Props.id} value="c" /><label htmlFor="number3">{Props.rep3}</label>
+      <div className="P2-input"><input type="radio" id={Props.id+"number3"} name={Props.id} value="c" onChange={Props.function} /><label htmlFor={Props.id+"number3"}>{Props.rep3}</label>
       </div>
     </div>
         </div>
--- src/pages/Quizz/index.jsx
+++ src/pages/Quizz/index.jsx
@@ -2,8 +2,101 @@ import { Helmet } from "react-helmet-async";
 import "../../style/pages/Quizz/quizz.scss"
 import Datas from "../../assets/Question.json"
 import QuestionP2 from "../../components/QuestionP2";
+import { useState } from "react";
 
 const Quizz = () => {
+
+
+  let allResponses = [];
+  let note = 0;
+
+  const Submit = (e) => {
+    e.preventDefault();
+    note = 0;
+    Note(Responses, responses);
+    showResults(note, responses);
+    error(Responses);
+  }
+
+ 
+
+  const getChecked = (e) => {
+    for (let i = 0; i < e.length; i++) {
+        if(e[i].checked === true) {
+            allResponses.push(e[i].value)
+            
+        }
+        
+    }   
+    }
+
+    const Note = (e, f) => {
+      for (let i = 0; i < e.length; i++) {
+  if(e[i] === f[i]) {
+      note++
+  }
+      }
+  }
+
+  const showResults = (e, f) => {
+    let noteForIndex = (e/f.length*5);
+    let noteIndex = null;
+switch(true) {
+    case(noteForIndex <= 1):
+noteIndex = 0;
+break;
+case (noteForIndex <= 2):
+    noteIndex = 1;
+    break;
+case (noteForIndex <= 4):
+    noteIndex = 2;
+    break;
+case (noteForIndex < 5):
+    noteIndex = 3;
+    break;
+case (noteForIndex === 5):
+    noteIndex = 4;
+    break;
+}
+updateConsignes((e) + '/' + f.length + ' ' + comment[noteIndex] +  ' ' + ' ' + emojis[noteIndex])
+}
+
+const error = (e) => {
+  for (let i = 0; i< e.length; i++) {
+      if(e[i] !== responses[i]) {
+          let wrongAnswerNumber = i+1
+          let wrongAnswer = document.getElementById(`q${wrongAnswerNumber}`);
+  wrongAnswer.classList.add("false")
+  wrongAnswer.classList.remove("reset")
+  wrongAnswer.classList.remove("correct")
+      }
+      else {
+          let goodAnswerNumber = i+1
+          let goodAnswer = document.getElementById(`q${goodAnswerNumber}`);
+          goodAnswer.classList.add("correct")
+          goodAnswer.classList.remove("reset")
+          goodAnswer.classList.remove("false")
+      }
+  }
+  }
+
+
+  const updateArray = () => {
+ const all = document.querySelectorAll("input[type='radio']")
+ allResponses = [];
+getChecked(all)
+updateResponses(allResponses)
+  }
+
+  const [Responses, updateResponses] = useState(["a", "a", "a", "a", "a", "a", "a", "a" , "a"])
+  const [Consignes, updateConsignes] = useState("")
+  
+
+
+  const responses = ["c", "a", "b", "a", "c", "a", "c", "b" , "a"];
+const emojis = ["👎", "😭" , "👀", "✨", "✔️" ];
+const comment = ["Catastrophique!", "Oula ce n'est pas bon", "Moyen", "Presque!", "Vous êtes excellent!"];
+
     return (
 <div className="P2-body">
     <Helmet>
@@ -20,16 +113,15 @@ const Quizz = () => {
       <div className="P2-bigCheck"></div>
     </div>
   </div>
-  <form method="get" action="" id="form">
+  <form method="get" action="" id="form" onSubmit={ (e) => Submit(e)}>
     {Datas.map(({id, title, rep1, rep2, rep3}) => 
-<QuestionP2 id={id} title={title} rep1={rep1} rep2={rep2} rep3={rep3} key={id} />
+<QuestionP2 id={id} title={title} rep1={rep1} rep2={rep2} rep3={rep3} key={id} function={updateArray} />
     )}
   <div className="P2-valider">
       <button className="P2-button">VALIDER <div>✔️</div>
       </button>
     </div>
-    <div className="P2-consignes" id="consignes">
-      Cliquez sur <span className="P2-boldy">valider</span> pour voir les <span className="P2-boldy">résultats.</span>
+    <div className="P2-consignes" id="consignes">{Consignes === "" ? <div>Cliquez sur <span className="P2-boldy">valider</span> pour voir les <span className="P2-boldy">résultats.</span></div> : Consignes}
     </div>
   </form>
 
