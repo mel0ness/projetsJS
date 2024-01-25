0c8ff8623c5e159bef975ee5574365ae26f0c192
prof <spasmodic51@gmail.com>
2024-01-25 18:17:15 +0100

    Size

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,12 +2,16 @@ import "../../style/pages/Home/home.scss"
 import TextTyping from "../../components/TextTyping";
 import {currentTheme } from "../../features/selector"
 import { useSelector } from "react-redux";
+import { Helmet } from "react-helmet-async";
 
 const Home = () => {
 
 const colors = useSelector(currentTheme)
     return (
         <div className={"home "+colors}>
+             <Helmet>
+                <title>Accueil</title>
+            </Helmet>
 <div className={"line-1 "+colors}><div className={"band band-1 "+colors}></div><div className="txt txt-1">Réalisations</div></div>
 <div className={"line-2 "+colors}><div className={"band band-2 "+colors}></div><div className="txt txt-2">de projets JS</div></div>
 <TextTyping />
--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -2,6 +2,7 @@ import "../../style/pages/Projects/projects.scss"
 import Projet from "../../components/Projet"
 import {currentTheme} from "../../features/selector"
 import { useSelector } from "react-redux"
+import { Helmet } from "react-helmet-async"
 
 
 const Projects = (Props) => {
@@ -9,6 +10,9 @@ const Projects = (Props) => {
     const colors = useSelector(currentTheme)
     return (
         <div className={"projets "+colors}>
+            <Helmet>
+                <title>Projets</title>
+            </Helmet>
 <div className="presProjets">Les projets sont directement intégrés en React-JS mais vous pouvez y trouver les liens pour le JS vanilla (Le code sur git comme le lien vers la page gitHub du projet)</div>
 
 {Props.Datas.map(({id, nom, adresse, GithubCode, GithubPage, description}) => 
