faef420c7a85bf2efa6a8904cfcb1a2a537e3276
prof <spasmodic51@gmail.com>
2024-05-21 16:37:47 +0200

    Interval semble fonctionner sans erreur console, reste à vérifier toutes les fonctionnalités de play pour un bon début

--- src/components/Timer/index.jsx
+++ src/components/Timer/index.jsx
@@ -1,25 +1,52 @@
-import { useEffect, useState } from "react";
+// import { useEffect} from "react";
 import "../../style/components/Timer/timer.scss"
 
 const Timer = (Props) => {
 
-useEffect(() => {
-    let timerFunction = setInterval(function()
-    {play(Props.minutes, Props.seconds, Props.isRunning, Props.moment, Props.momentState, Props.momentStateFunction, timerFunction)}, 1000)
-}
-        , [Props.isRunning, Props.moment, Props.minutes, Props.seconds, Props.momentState, Props.momentStateFunction])
+// useEffect(() => {
+//     let timerFunction = setInterval(function()
+//     {play(Props.minutes,Props.updateMinutes, Props.seconds, Props.updateSeconds, Props.isRunning, Props.moment, Props.momentState, Props.momentStateFunction, timerFunction)}, 1000)
+// }
+//         , [Props.isRunning, Props.moment, Props.minutes, Props.updateMinutes, Props.seconds, Props.updateSeconds, Props.momentState, Props.momentStateFunction])
 
-const play = (minutes, seconds, isRunning, moment, momentState, momentStateFunction, interval) => {
+// const intervalBegin = () => {
+//     let timerFunction = setInterval(function()
+//     {play(Props.minutes,Props.updateMinutes, Props.seconds, Props.updateSeconds, Props.isRunning, Props.moment, Props.momentState, Props.momentStateFunction, timerFunction)}, 1000)
+// }
 
-}        
-    
-const [minutes, updateMinutes] = useState(Props.minutes)
-const [seconds, updateSeconds] = useState(Props.seconds)
+// const play = (minutes, updateMinutes, seconds, updateSeconds, isRunning, moment, momentState, momentStateFunction, interval) => {
+//     if(!isRunning || moment !== momentState) {
+//         clearInterval(interval)
+//     }
+
+//    else if(minutes >= 0 && seconds ===0) {
+//         updateMinutes(minutes - 1);
+//         updateSeconds(59);
+//     }
+//     else if(minutes >= 0 && seconds > 0) {
+//         updateSeconds(seconds - 1);
+//     }
+//     else {
+//         if(momentState === "Travail"){
+//             updateMinutes(30)
+//             updateSeconds(0)
+//             momentStateFunction("Repos")
+//             clearInterval(interval)
+//         }
+//         else {
+//             updateMinutes(5)
+//             updateSeconds(0)
+//         momentStateFunction("Travail")
+//             clearInterval(interval)
+//         }
+       
+//     }
+// }        
 
     return(
         <div className="P7-timer">
       <div className="P7-timeTitle">{Props.moment}</div>
-      <div className="P7-timeUnits"><span>{minutes}</span>:{seconds < 10? <span>0{seconds}</span> : <span>{seconds}</span>}</div>
+      <div className="P7-timeUnits"><span>{Props.minutes}</span>:{Props.seconds < 10? <span>0{Props.seconds}</span> : <span>{Props.seconds}</span>}</div>
     </div>
     )
 }
--- src/pages/Pomodoro/index.jsx
+++ src/pages/Pomodoro/index.jsx
@@ -4,14 +4,71 @@ import Pause from "../../assets/pause.svg"
 import Reset from "../../assets/reset.svg"
 import Timer from "../../components/Timer"
 import { Helmet } from "react-helmet-async";
-import { useState } from "react"
+import { useState, useEffect } from "react"
 const Pomodoro = () => {
 
-const [time, updateTime] = useState("Travail")
-const [running, updateRunning] = useState(false)
+    const [time, updateTime] = useState("Travail")
+const [running, updateRunning] = useState(true)
 const [cycles, updateCycles] = useState(0)
-const [workTimer] = useState([30, 0])
-const [breakTimer] = useState([5, 0])
+const [workTimerMinutes, updateWorkTimerMinutes] = useState(30)
+const [workTimerSeconds, updateWorkTimerseconds] = useState(0)
+const [breakTimerMinutes, updateBreakTimerMinutes] = useState(5)
+const [breakTimerSeconds, updateBreakTimerseconds] = useState(0)
+
+    useEffect(() => {
+
+        const play = (interval) => {
+            if(running === false) {
+                clearInterval(interval)
+            }
+        
+           else if(time === "Travail" && workTimerMinutes >=0 && workTimerSeconds ===0) {
+                updateWorkTimerMinutes(workTimerMinutes - 1);
+                updateWorkTimerseconds(59);
+            }
+            else if( time === "Travail" && workTimerMinutes >= 0 && workTimerSeconds > 0) {
+                updateWorkTimerseconds(workTimerSeconds - 1);
+            }
+           else if(time === "Repos" && breakTimerMinutes >=0 && breakTimerSeconds ===0) {
+                updateBreakTimerMinutes(breakTimerMinutes - 1);
+                updateBreakTimerseconds(59);
+            }
+            else if( time === "Repos" && breakTimerMinutes >= 0 && breakTimerSeconds > 0) {
+                updateBreakTimerseconds(breakTimerSeconds - 1);
+            }
+            else {
+                if(time === "Travail"){
+                    updateWorkTimerMinutes(30)
+                    updateWorkTimerseconds(0)
+                    updateTime("Repos")
+                }
+                else {
+                    updateBreakTimerMinutes(5)
+                    updateBreakTimerseconds(0)
+                updateTime("Travail")
+                }
+               
+            }
+        }   
+
+        const timerFunction = setInterval(() =>
+        {play(timerFunction)}, 1000);
+        return () => clearInterval(timerFunction);
+    }
+            , [breakTimerMinutes, breakTimerSeconds, workTimerMinutes, workTimerSeconds, updateBreakTimerMinutes, updateBreakTimerseconds, updateWorkTimerMinutes, updateWorkTimerseconds, time, running])
+    
+
+
+
+
+const startRunning = () => {
+    updateRunning(true)
+}
+const stopRunning = () => {
+    updateRunning(false)
+}
+
+
 
     return (
         <div className="P7-body">
@@ -22,13 +79,13 @@ const [breakTimer] = useState([5, 0])
   <div className="P7-title">Le <span className="P7-bold">POMODORO</span></div>
 
   <div className="P7-timers">
-   <Timer moment="Travail" minutes={workTimer[0]} seconds={workTimer[1]} momentState={time} momentStateFunction={updateTime} isRunning={running} cycles={cycles} updateCycles={updateCycles} />
-   <Timer moment="Repos" minutes={breakTimer[0]} seconds={breakTimer[1]} momentState={time} momentStateFunction={updateTime} isRunning={running} cycles={cycles} updateCycles={updateCycles} />
+   <Timer moment="Travail" minutes={workTimerMinutes} updateMinutes={updateWorkTimerMinutes} seconds={workTimerSeconds} updateSeconds={updateWorkTimerseconds} momentState={time} momentStateFunction={updateTime} isRunning={running} cycles={cycles} updateCycles={updateCycles} />
+   <Timer moment="Repos" minutes={breakTimerMinutes} updateMinutes={updateBreakTimerMinutes} seconds={breakTimerSeconds} updateSeconds={updateBreakTimerseconds} momentState={time} momentStateFunction={updateTime} isRunning={running} cycles={cycles} updateCycles={updateCycles} />
   </div>
 
   <div className="P7-actions">
     <div className="P7-action" id="play">
-        {running? <img src={Pause} className="P7-play" id="playIMG" alt="play/pause" onClick={() => updateRunning(false)} /> :<img src={Play} className="P7-play" id="playIMG" alt="play/pause" onClick={() => updateRunning(true)} />}
+        {running? <img src={Pause} className="P7-play" id="playIMG" alt="play/pause" onClick={() => stopRunning()} /> :<img src={Play} className="P7-play" id="playIMG" alt="play/pause" onClick={() => startRunning()} />}
     </div>
     <div className="P7-action" id="reset"><img src={Reset} className="P7-reset" alt="reset" /></div>
   </div>
